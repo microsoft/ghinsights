ad0504149785e7200a1183ba6a8bfbbac5e0cb88
Kevin Lewis <kelewis@microsoft.com>
2016-01-18 00:10:03 -0800

    Initial checkin of GitHubAnalytics
    Currently contains an ADF pipline to investigate moving MongoDB dumps into AzureDataLake.  At the moment it will untar and save the files back into Blob storage.  Investigating further to see if the data can be saved directly to ADL, but login/auth to ADL needs to be figured out first.
    
    Checking in for now since the initial concept of custom batch job untaring source is currently working.

---
+++ GitHubAnalytics/DataFactoryActivityExec/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.DataFactories;
+using Microsoft.Azure.Management.DataFactories.Common.Models;
+using Microsoft.Azure.Management.DataFactories.Core.Registration.Models;
+using Microsoft.Azure.Management.DataFactories.Models;
+using Microsoft.Azure.Management.DataFactories.Runtime;
+using Newtonsoft.Json.Linq;
+
+namespace DataFactoryActivityExec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customActivity = new MongoDbDumpTransformActivity.MongoDbDumpTransformActivity();
+
+
+            var linkedServices = new List<LinkedService>()
+            {   new LinkedService("GHTorrentAzureStorage",
+                    new LinkedServiceProperties(new AzureStorageLinkedService("DefaultEndpointsProtocol=https;AccountName=kelewis;AccountKey=hCvPmKWV532FXwo+S1XvTeq64QDt/ibWdewU2oHDSrs9slXaHIHpbxUCHL99WF/1O4AR53WhHHOKu2BKf3UNbA==")))
+                ,new LinkedService("GitHubAnalyticsAzureStorage",
+                    new LinkedServiceProperties(new AzureStorageLinkedService("DefaultEndpointsProtocol=https;AccountName=kelewis;AccountKey=hCvPmKWV532FXwo+S1XvTeq64QDt/ibWdewU2oHDSrs9slXaHIHpbxUCHL99WF/1O4AR53WhHHOKu2BKf3UNbA==")))
+            };
+            
+            var mongoDbDump = new Dataset("MongoDbDump",
+                new DatasetProperties(new AzureBlobDataset()
+                {
+                    FolderPath = "downloads/",
+                    FileName = "mongo-dump-{Year}-{Month}-{Day}.tar.gz",
+                    PartitionedBy = new List<Partition>()
+                    {
+                        {
+                            new Partition()
+                            {
+                                Name = "Year",
+                                Value = new DateTimePartitionValue() {Date = "SliceStart", Format = "yyyy"}
+                            }
+                        },
+                        {
+                            new Partition()
+                            {
+                                Name = "Month",
+                                Value = new DateTimePartitionValue() {Date = "SliceStart", Format = "MM"}
+                            }
+                        },
+                        {
+                            new Partition()
+                            {
+                                Name = "Day",
+                                Value = new DateTimePartitionValue() {Date = "SliceStart", Format = "dd"}
+                            }
+                        }
+                    }
+                },
+                    new Availability("Daily", 1), "GHTorrentAzureStorage"));
+
+            var eventDetailRawFilesBlob = new Dataset("EventDetailRawFilesBlob",
+                new DatasetProperties(new AzureBlobDataset()
+                {
+                    FolderPath = @"raw/MongoDump/V1/{EventName}/{Year}/{Month}",
+                   FileName = "{EventName}_{Year}_{Month}_{Day}.json.gz",
+                    PartitionedBy = new List<Partition>()
+                    {
+                        {
+                            new Partition()
+                            {
+                                Name = "Year",
+                                Value = new DateTimePartitionValue() {Date = "SliceStart", Format = "yyyy"}
+                            }
+                        },
+                        {
+                            new Partition()
+                            {
+                                Name = "Month",
+                                Value = new DateTimePartitionValue() {Date = "SliceStart", Format = "MM"}
+                            }
+                        },
+                        {
+                            new Partition()
+                            {
+                                Name = "Day",
+                                Value = new DateTimePartitionValue() {Date = "SliceStart", Format = "dd"}
+                            }
+                        }
+                    }
+                },
+                    new Availability("Daily", 1), "GitHubAnalyticsAzureStorage"));
+            
+
+            var datasets = new List<Dataset>() { mongoDbDump, eventDetailRawFilesBlob };
+
+
+            var activity = new Activity()
+            {
+                Description = "<Enter the Pipeline description here>",
+                Inputs = new ActivityInput[] {new ActivityInput("MongoDbDump")},
+                LinkedServiceName = "BatchProcessor",
+                Name = "GHTorrentEventDetailPipeline",
+                Outputs = new ActivityOutput[] {new ActivityOutput("EventDetailRawFilesBlob") },
+                Policy =
+                    new ActivityPolicy()
+                    {
+                        Concurrency = 4,
+                        ExecutionPriorityOrder = "NewestFirst",
+                        Retry = 3,
+                        Timeout = TimeSpan.Parse("04:00:00")
+                    },
+                Scheduler = new Scheduler("Day", 1),
+                //Type = "MongoDbDumpTransformActivity",
+                TypeProperties = new DotNetActivity("MongoDbDumpTransformActivity.dll"
+                    , "MongoDbDumpTransformActivity.MongoDbDumpTransformActivity"
+                    , "datafactory/MongoDbDumpTransformActivity.zip"
+                    , "GitHubAnalyticsAzureStorage")
+                {
+                    ExtendedProperties = new Dictionary<string, string>()
+                    {
+                        {"Year", "2015"},
+                        {"Month", "12"},
+                        {"Day", "02"}
+                    }
+                }
+            };
+
+            IActivityLogger logger = new DebugLogger();
+
+
+
+            customActivity.Execute(linkedServices, datasets, activity, logger);
+        }
+    }
+
+    class DebugLogger : IActivityLogger
+    {
+        public void Write(string format, params object[] args)
+        {
+            Debug.WriteLine(String.Format(format,args));
+        }
+    }
+}
---
+++ GitHubAnalytics/MongoDBDumpTransformActivity/MongoDbDumpTransformActivity.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.Azure.Management.DataFactories.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.Azure.Management.DataFactories;
+using Microsoft.Azure.Management.DataFactories.Models;
+using Microsoft.Azure.Management.DataLake.StoreFileSystem;
+using Microsoft.Azure.Management.DataLake.StoreUploader;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using System.Web;
+using Hyak.Common;
+using Newtonsoft.Json.Bson;
+using Newtonsoft.Json.Linq;
+
+namespace MongoDbDumpTransformActivity
+{
+    public class MongoDbDumpTransformActivity : IDotNetActivity
+    {
+        /// <summary>
+        /// Execute method is the only method of IDotNetActivity interface you must implement. 
+        /// In this sample, the method invokes the Calculate method to perform the core logic.  
+        /// </summary>
+
+        public IDictionary<string, string> Execute(
+            IEnumerable<LinkedService> linkedServices,
+            IEnumerable<Dataset> datasets,
+            Activity activity,
+            IActivityLogger logger)
+        {
+            /////////////////
+            // Log input parameters
+
+            // to get extended properties (for example: SliceStart)
+            foreach (LinkedService ls in linkedServices)
+            {
+                logger.Write("linkedServices: {0}, {1}, {2}, {3}", ls.Name, ls.Properties.Type,
+                    ls.Properties.Description, ls.Properties.ErrorMessage);
+            }
+            //logger.Write("datasets: {0}", JsonConvert.SerializeObject(datasets));
+            //logger.Write("activity: {0}", JsonConvert.SerializeObject(activity));
+
+            var sliceYear = ((DotNetActivity) activity.TypeProperties).ExtendedProperties["Year"];
+            var sliceMonth = ((DotNetActivity)activity.TypeProperties).ExtendedProperties["Month"];
+            var sliceDay = ((DotNetActivity)activity.TypeProperties).ExtendedProperties["Day"];
+
+            /////////////////
+            // Open up input Blob
+
+            var inputDataset = datasets.Single(dataset => dataset.Name == activity.Inputs.Single().Name);
+            
+            var inputLinkedService = linkedServices.Single(
+                linkedService =>
+                linkedService.Name ==
+                inputDataset.Properties.LinkedServiceName).Properties.TypeProperties
+                as AzureStorageLinkedService;
+
+            var inConnectionString = inputLinkedService.ConnectionString; // To create an input storage client.
+            var inContainerName = GetContainerName(inputDataset);
+            var inFolderPath = GetFolderPath(inputDataset, sliceYear, sliceMonth, sliceDay);
+            var inFileName = GetFileName(inputDataset, sliceYear, sliceMonth, sliceDay);
+            
+
+            CloudStorageAccount inputStorageAccount = CloudStorageAccount.Parse(inConnectionString);
+            CloudBlobClient inputClient = inputStorageAccount.CreateCloudBlobClient();
+
+            var inputBlobUri = new Uri(inputStorageAccount.BlobEndpoint, inContainerName + (String.IsNullOrWhiteSpace(inFolderPath)?null:"/" + inFolderPath) + "/" + inFileName);
+            var sourceBlob = inputClient.GetBlobReferenceFromServer(inputBlobUri);
+
+            ////////////////
+            // Get output location
+
+
+            var outputDataset = datasets.Single(dataset => dataset.Name == activity.Outputs.Single().Name);
+
+            var outputLinkedService = linkedServices.Single(
+                linkedService =>
+                linkedService.Name ==
+                outputDataset.Properties.LinkedServiceName).Properties.TypeProperties
+                as AzureStorageLinkedService;
+
+            var outConnectionString = outputLinkedService.ConnectionString; // To create an input storage client.
+            var outFolderPath = GetFolderPath(outputDataset, sliceYear, sliceMonth, sliceDay);
+            var outFileName = GetFileName(outputDataset, sliceYear, sliceMonth, sliceDay);
+            var outContainerName = GetContainerName(outputDataset);
+
+
+            CloudStorageAccount outputStorageAccount = CloudStorageAccount.Parse(outConnectionString);
+            CloudBlobClient outputClient = outputStorageAccount.CreateCloudBlobClient();
+            var outContainer = outputClient.GetContainerReference("raw");
+            outContainer.CreateIfNotExists();
+
+            //format output path string
+            var outputFilenameFormatString = String.Concat(outFolderPath, "/", outFileName).Replace("{EventName}", "{0}");
+
+
+
+
+
+            using (var sourceBlobStream = sourceBlob.OpenRead())
+            using (var unZipStream = new System.IO.Compression.GZipStream(sourceBlobStream, System.IO.Compression.CompressionMode.Decompress))
+            using (var tarStream = new TarStream.TarStream(unZipStream))
+            {
+
+                logger.Write("BlobRead: {0}", sourceBlob.Name);
+                do
+                {
+                    var taredFileName = new System.IO.FileInfo(tarStream.CurrentFilename);
+
+                    if (taredFileName.Extension == ".bson")
+                    {
+                        var tableName = taredFileName.Name.Split('.')[0];
+                        var outputBlob = outContainer.GetBlockBlobReference(String.Format(outputFilenameFormatString,tableName));
+
+                        using (var outBlobStream = outputBlob.OpenWrite())
+                        using (var gzipOut = new GZipStream(outBlobStream, System.IO.Compression.CompressionLevel.Optimal))
+                        using (var outText = new StreamWriter(gzipOut, Encoding.UTF8))
+                        using (var reader = new BsonReader(tarStream))
+                        {
+
+                            logger.Write("BlobWrite: {0}/{1}", outContainerName, outputBlob.Name);
+
+                            reader.CloseInput = false;
+
+                            var jsonSerializer = new JsonSerializer();
+
+
+                            reader.ReadRootValueAsArray = false;
+                            reader.SupportMultipleContent = true;
+
+                            while (reader.Read())
+                            {
+                                var row = (JObject)jsonSerializer.Deserialize(reader);
+
+                                var outString = row.ToString(Formatting.None);
+
+                                outText.WriteLine(outString);
+                            }
+                        }
+                    }
+
+                } while (tarStream.NextFile());
+            }
+
+
+
+            // return a new Dictionary object (unused in this code).
+            return new Dictionary<string, string>();
+        }
+        /// <summary>
+        /// Gets the folderPath value from the input/output dataset.   
+        /// </summary>
+
+        private static string GetFolderPath(Dataset dataArtifact, string sliceYear, string sliceMonth, string sliceDay)
+        {
+            if (dataArtifact == null || dataArtifact.Properties == null)
+            {
+                return null;
+            }
+
+            AzureBlobDataset blobDataset = dataArtifact.Properties.TypeProperties as AzureBlobDataset;
+            if (blobDataset == null)
+            {
+                return null;
+            }
+
+            return blobDataset.FolderPath.Replace(GetContainerName(dataArtifact),"").TrimStart('/').TrimEnd('/').Replace("{Year}", sliceYear).Replace("{Month}", sliceMonth).Replace("{Day}", sliceDay);
+        }
+
+
+        /// <summary>
+        /// Gets the fileName value from the input/output dataset.   
+        /// </summary>
+
+        private static string GetFileName(Dataset dataArtifact, string sliceYear, string sliceMonth, string sliceDay)
+        {
+            if (dataArtifact == null || dataArtifact.Properties == null)
+            {
+                return null;
+            }
+
+            AzureBlobDataset blobDataset = dataArtifact.Properties.TypeProperties as AzureBlobDataset;
+            if (blobDataset == null)
+            {
+                return null;
+            }
+
+            return blobDataset.FileName.Replace("{Year}", sliceYear).Replace("{Month}", sliceMonth).Replace("{Day}", sliceDay);
+        }
+        private static string GetContainerName(Dataset dataArtifact)
+        {
+            if (dataArtifact == null || dataArtifact.Properties == null)
+            {
+                return null;
+            }
+
+            AzureBlobDataset blobDataset = dataArtifact.Properties.TypeProperties as AzureBlobDataset;
+            if (blobDataset == null)
+            {
+                return null;
+            }
+
+            return blobDataset.FolderPath.Split('/')[0];
+        }
+
+    }
+}
